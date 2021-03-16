dbeeeae8431771173d4f28b62db8c6a3a9230fbd
Jochem Leunisse <jleunisse@gmail.com>
2021-03-16 18:18:54 +0100

    added routing with express

--- src/expresstest.js
+++ src/expresstest.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Hello World!!");
+  res.send("Welcome to Padhop!");
+});
+app.get("/about", (req, res) => {
+  res.send("Welcome to the about page!");
+});
+app.get("/home", (req, res) => {
+  res.send("Welcome to your homepage!");
 });
 
 app.listen(port, () => {
